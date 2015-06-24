f6be3af56cf91da66543bc9eb7e5ab8b003d1268
artsoroka <githubrepository@yandex.ru>
2015-06-24 06:24:34 -0400

    db connection charset setting

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'csuser',
   password : 'cspassword', 
-  database : 'cs'
+  database : 'cs', 
+  charset  : 'utf8_unicode_ci'
 });
 
 connection.connect();
@@ -19,14 +20,24 @@ util.inherits(DataBase, EventEmitter);
 
 module.exports = DataBase; 
 
+DataBase.prototype.getAll = function(callback){
+  connection.query("SELECT * FROM game_bets", function(err, rows){
+    if(err) return console.log('db err: ', err); 
+    callback(rows); 
+  });
+}; 
+
 DataBase.prototype.newBid = function(entry){
   var self  = this; 
   var ts    = Date.now() / 1000 | 0; 
+
+  console.log('new entry: ', entry); 
+  var userId = JSON.parse( JSON.stringify( entry.userId ) ); 
   var sql   = 'INSERT INTO game_bets(id, user_id, artifact_id, timestamp) VALUES(null, ?, ?, ?)'; 
   connection.query(sql, [entry.userId, entry.artifactId, ts], function(err, rows) {
     if (err) throw err;
     self.emit('newBid', entry); 
-    console.log('The solution is: ', rows);
+    console.log('new db entry: ', rows);
   });
 
 };
